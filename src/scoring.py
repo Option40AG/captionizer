0575db1a999cd2888a62defd3386eb84f3b6d748
Luis Delgado <luis.delgado@fifa.org>
2023-05-30 19:58:13 -0600

    Adds statement to check for GPU presence

--- src/scoring.py
+++ src/scoring.py
@@ -1,14 +1,25 @@
 from transformers import AutoProcessor, AutoModelForCausalLM
 import requests
+import torch
 from PIL import Image
 
 def score(image: bytes):
 
     # Use assets of the model from the local filesystem
     processor = AutoProcessor.from_pretrained("./../model/git-base-textcaps")
-    model = AutoModelForCausalLM.from_pretrained("./../model/git-base-textcaps")
 
-    pixel_values = processor(images=image, return_tensors="pt").pixel_values
+    if torch.cuda.is_available():
+        print("Running on cuda")
+        device = torch.device("cuda")
+        model = AutoModelForCausalLM.from_pretrained("./../model/git-base-textcaps").to(device)
+    else:
+        print("Running on cpu")
+        device = torch.device("cpu")
+        model = AutoModelForCausalLM.from_pretrained("./../model/git-base-textcaps").to(device)
+
+    # model = AutoModelForCausalLM.from_pretrained("./../model/git-base-textcaps")
+
+    pixel_values = processor(images=image, return_tensors="pt").pixel_values.to(device)
 
     generated_ids = model.generate(pixel_values=pixel_values, max_length=50)
     generated_caption = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
